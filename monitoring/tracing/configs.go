6540e8b56900f7b45e6664389a142d5ac9ded04c
the-witcher-knight <dhuuloc8818@gmail.com>
2025-03-16 17:31:34 +0700

    Improve tracing resource config

--- monitoring/tracing/configs.go
+++ monitoring/tracing/configs.go
@@ -6,6 +6,9 @@ import (
 
 // Config holds the configuration for setting up OpenTelemetry components.
 type Config struct {
+	ServerName    string
+	Environment   string
+	Version       string
 	ExporterURL   string        // URL of the OTLP exporter
 	TransportType TransportType // Transport type: HTTP or gRPC
 	TLSConfig     *tls.Config   // TLS configuration for secure transport
